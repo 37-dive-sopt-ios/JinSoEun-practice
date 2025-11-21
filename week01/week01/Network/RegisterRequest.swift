45cf511a3dd9e89f2c8452c4a5621c54ea79f887
jinsoeun <rosejinse@naver.com>
2025-11-21 23:45:19 +0900

    [feat] 4차 세미나 실습

---
+++ week01/week01/Network/RegisterRequest.swift
@@ -0,0 +1,14 @@
+//
+//  RegisterRequest.swift
+//  Baemin
+//
+//  Created by 진소은 on 11/8/25.
+//
+
+//struct RegisterRequest: Encodable {
+//    let username: String
+//		let password: String
+//		let name: String
+//		let email: String
+//		let age: Int
+//}
